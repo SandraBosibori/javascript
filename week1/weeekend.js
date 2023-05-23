cbfdf43dcc704a66dd4b1d6952743541709101fd
SandraBosibori <sandrahbosibori@gmail.com>
2023-05-23 10:31:03 +0300

    new changes

--- week1/weeekend.js
+++ week1/weeekend.js
@@ -1,6 +1,26 @@
 let a= [
-    {title:''}
-    {date:}
-    {location:''}
-    {atendees:}
+    {
+     title:'Lesson 1'
+     date:new Date(19-05-2023)
+     location:'Nairobi'
+     atendees:newSet(Jane, Chris, Anette)
+    },
+
+    {
+     title:'Lesson 2'
+     date:new Date(20-05-2023)
+     location:'Kisumu'
+     atendees:newSet(Janet, Austine, Ken)
+    },
+
+    {
+     title:'Lesson 3'
+     date:new Date(21-05-2023)
+     location:'Mombasa'
+     atendees:newSet(Paul, Kay, Emily)
+    },
 ]
+
+Array.filter(function(a, 0, ))
+
+let a.map(function(a{}))
---
+++ week2/day2.js
@@ -0,0 +1,9 @@
+const button1=document.querySelector('#one')
+const button2=document.querySelector('#two')
+
+
+
+let button1Color = button1.style.backgroundColor
+let button2Color = button2.style.backgroundColor
+
+document.button1.addEventListener('click',function(){document.body.style.backgroundColor="button1Color"})
